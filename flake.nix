06b773de830491b6c7501bbc40108ca603e3e4ca
David Poole <dtpoole@users.noreply.github.com>
2026-01-09 15:20:18 -0500

    switch back to main

--- flake.nix
+++ flake.nix
@@ -1,7 +1,7 @@
 {
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    nvf.url = "github:notashelf/nvf/v0.8";
+    nvf.url = "github:notashelf/nvf/main";
   };
   outputs = {
     nixpkgs,
